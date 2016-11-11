31a5c763b69592ee54511f152d8cba84d04ad7b7
steven <smt529@york.ac.uk>
2016-11-11 09:49:43 +0000

    Add io support

--- test/desktop/src/me/gandhiinc/test/desktop/DesktopLauncher.java
+++ test/desktop/src/me/gandhiinc/test/desktop/DesktopLauncher.java
@@ -7,8 +7,14 @@ import me.gandhiinc.test.Game;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		//config.fullscreen = true;
 		config.width = 1280;
 		config.height = 720;
-		new LwjglApplication(new Game(), config);
+		config.backgroundFPS = 0;
+		config.foregroundFPS = 0;
+		config.vSyncEnabled = true;
+		config.title = "Blind Eye";
+		Game game = new Game();
+		LwjglApplication app = new LwjglApplication(game, config);
 	}
 }
