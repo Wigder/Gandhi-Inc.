ba553bdc550ee2138915ec161d5d71c77e480533
Robert Sayles <ras570@york.ac.uk>
2016-11-15 12:58:32 +0000

    As before, take 2

--- test/core/src/me/gandhiinc/test/Roboticon.java
+++ test/core/src/me/gandhiinc/test/Roboticon.java
@@ -7,11 +7,8 @@ package me.gandhiinc.test;
  * @author Robert
  *
  */
-
 class Roboticon {
-	enum resource {
-		none, ore, energy
-	}
+	// Setting the standard production rate as a constant for reference during specialisation
 	private static int BASEPROD = 2;
 	/* Store the roboticon's base production for each resource
 	 * Handling production as integers internally to avoid potential decimal inaccuracies
@@ -19,33 +16,46 @@ class Roboticon {
 	 */
 	private int OreProd = 0;
 	private int EnergyProd = 0;
-	private resource Specialisation = resource.none;
+	private Resource Specialisation = Resource.NONE;
 	
-	// Constructor for roboticons
+	/**
+	 * Constructor for roboticons
+	 */
 	Roboticon () {
 		
 	}
 	
-	// Returns the base production value for the given resource
-	int getBaseProd (resource resource) {
+	/**
+	 * Returns the base production value for a given resource
+	 * 
+	 * @param resource	the resource for which the base production is being requested as an enumerated type
+	 * @return			the production rate for requested resource as an integer
+	 */
+	int getBaseProd (Resource resource) {
 		switch (resource) {
-		case ore:
+		case ORE:
 			return OreProd;
-		case energy:
+		case ENERGY:
 			return EnergyProd;
 		default:
 			return 0;
 		}
 	}
 	
-	// Sets the base production for the given resource to the given value
-	boolean setBaseProd (resource resource, int newprod) {
+	/**
+	 * Sets the base production for the given resource to the given value
+	 * 
+	 * @param resource	the resource for which the base production rate is being set as an enumerated type
+	 * @param newprod	the new production rate as an integer
+	 * @return			a boolean for success of the operation
+	 */
+	boolean setBaseProd (Resource resource, int newprod) {
 		if (newprod >= 0) {
 			switch (resource) {
-		case ore:
+		case ORE:
 			this.OreProd = newprod;
 			return true;
-		case energy:
+		case ENERGY:
 			this.EnergyProd = newprod;
 			return true;
 		default:
@@ -57,21 +67,27 @@ class Roboticon {
 		
 	}
 	
-	// Sets the specialisation to the given resource and sets the base production accordingly
-	boolean setSpecialisation (resource resource) {
+	/**
+	 * Takes an enumerated resource type and sets the specialisation
+	 * and base production rates accordingly
+	 * 
+	 * @param resource	the resource being set as an enumerated type
+	 * @return			a boolean for success of the operation
+	 */
+	boolean setSpecialisation (Resource resource) {
 		switch (resource) {
-		case none:
-			this.Specialisation = resource.none;
+		case NONE:
+			this.Specialisation = Resource.NONE;
 			this.OreProd = 0;
 			this.EnergyProd = 0;
 			return true;
-		case ore:
-			this.Specialisation = resource.ore;
+		case ORE:
+			this.Specialisation = Resource.ORE;
 			this.OreProd = BASEPROD;
 			this.EnergyProd = 0;
 			return true;
-		case energy:
-			this.Specialisation = resource.energy;
+		case ENERGY:
+			this.Specialisation = Resource.ENERGY;
 			this.OreProd = 0;
 			this.EnergyProd = BASEPROD;
 			return true;
@@ -80,8 +96,12 @@ class Roboticon {
 		}
 	}
 	
-	// Returns the current specialisation
-	resource getSpecialisation () {
+	/**
+	 * Returns the current specialisation as an enumerated resource type
+	 * 
+	 * @return 			the current specialisation as an enumerated resource type
+	 */
+	Resource getSpecialisation () {
 		return this.Specialisation;
 	}
 }
