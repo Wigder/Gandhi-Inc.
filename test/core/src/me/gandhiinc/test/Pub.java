82e066225b762d7085ed4f36f9e3eacb02a18537
LR Jenkinson <lrj509@pc681s.cs.york.ac.uk>
2016-11-17 16:09:13 +0000

    luke update

--- test/core/src/me/gandhiinc/test/Pub.java
+++ test/core/src/me/gandhiinc/test/Pub.java
@@ -1,30 +1,33 @@
 package me.gandhiinc.test;
 
-public class Pub
-{
-    int priceOfPlayingLottery;
-    int priceOfPlayingScratchCard;
-    int priceOfPlayingOneArmBandit;
+/**
+ *@author Luke Jenkinson
+ *@version 1.0
+ */
+
+
+public class Pub{
+    private int priceOfPlayingLottery;
+    private int priceOfPlayingScratchCard;
+    private int priceOfPlayingOneArmBandit;
 
     public static void main(String args[]){
         Pub instance = new Pub();
+        instance.getPriceOfPlayingLottery;
     }
 
-    public int getPriceOfPlayingLottery()
-    {
+    public int getPriceOfPlayingLottery(){
     	System.out.println(priceOfPlayingLottery);
         return priceOfPlayingLottery;
         
     }
 
-    public int getPriceOfPlayingScratchCard()
-    {
+    public int getPriceOfPlayingScratchCard(){
     	System.out.println(priceOfPlayingScratchCard);
         return priceOfPlayingScratchCard;
     }
 
-    public int getPriceOfPlayingOneArmBandit()
-    {
+    public int getPriceOfPlayingOneArmBandit(){
         System.out.println(priceOfPlayingOneArmBandit);
         return priceOfPlayingOneArmBandit;
     }
