267e42c066197d11d9d05ba5e5c46fd6ac04b0f0
LR Jenkinson <lrj509@pc643s.cs.york.ac.uk>
2016-11-18 10:10:07 +0000

    more added to pub

--- test/core/src/me/gandhiinc/test/Pub.java
+++ test/core/src/me/gandhiinc/test/Pub.java
@@ -1,4 +1,5 @@
 /**
+ *Represents the Pub methods, ie the gambling system of the game.
  *@author Luke Jenkinson
  *@version 1.0
  */
@@ -14,21 +15,46 @@ public class Pub{
         instance.getPriceOfPlayingLottery();
     }
 
+    /**
+    *
+    *@return priceOfPlayingLottery This returns the price of playing the lottery 
+    *
+    **/
     public int getPriceOfPlayingLottery(){
     	System.out.println(priceOfPlayingLottery);
         return priceOfPlayingLottery;
         
     }
 
+    /**
+    *
+    *@return priceOfPlayingScratchcard returns the price of playing a scratchcard
+    *
+    **/
     public int getPriceOfPlayingScratchCard(){
     	System.out.println(priceOfPlayingScratchCard);
         return priceOfPlayingScratchCard;
     }
 
+    /**
+    *
+    *@return priceOfPlayingOneArmBandit returns the price of playing one armed bandit
+    *
+    **/
+
     public int getPriceOfPlayingOneArmBandit(){
         System.out.println(priceOfPlayingOneArmBandit);
         return priceOfPlayingOneArmBandit;
     }
+
+    /**
+    *
+    *@param number1 the first number in the lottery, should be between 1 and 20 incluseive
+    *@param number2 the second number in the lottery, should be between 1 and 20 inclusive
+    *@param number3 the third number in the lottery, should be between 1 and 20 inclusive
+    *@return money returns the amount of money returned to the playe4r, will be 0 for loss
+    *
+    **/
 
     public playLottery(number1, number2, number3){
         if(number1 == number2)throw new IlligalArgumentException();
