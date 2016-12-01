163d16dd0a39b4fb67bea146fc81eaf639963fbf
Wigder <Wigder@users.noreply.github.com>
2016-12-01 16:20:46 +0000

    Update Plot.java

--- core/src/me/gandhiinc/blindeye/Plot.java
+++ core/src/me/gandhiinc/blindeye/Plot.java
@@ -125,45 +125,4 @@ public class Plot
 		return plotRoboticon;
 	}
 	
-	//Fixed plots from x, y coordinates
-	//Need to create the plot objects and fix their coordinates will do this next Friday perhaps
-	//We will have to fix their terrain type and modifiers also
-	
-	//TOP ROW STARTING FROM LEFT
-	
-	//Plot 1: if(x < 175 && y < 175)
-	//Plot 2: if(175 < x < 349 && y < 175)
-	//Plot 3: if(349 < x < 523 && y < 175)
-	//Plot 4: if(523 < x < 697 && y < 175)
-	//Plot 5: if(697 < x < 870 && y < 175)
-	//Plot 6: if(870 < x < 1043 && y < 175)
-	
-	//SECOND ROW STARTING FROM LEFT
-	
-	//Plot 7: if(x < 175 && 175 < y < 349)
-	//Plot 8: if(175 < x < 349 && 175 < y < 349)
-	//Plot 9: if(349 < x < 523 && 175 < y < 349)
-	//Plot 10: if(523 < x < 697 && 175 < y < 349)
-	//Plot 11: if(697 < x < 870 && 175 < y < 349)
-	//Plot 12: if(870 < x < 1043 && 175 < y < 349)
-	
-	// THIRD ROW STARTING FROM LEFT
-	
-	//Plot 13: if(x < 175 && 349 < y < 523)
-	//Plot 14: if(175 < x < 349 && 349 < y < 523)
-	//Plot 15: if(349 < x < 523 && 349 < y < 523)
-	//Plot 16: if(523 < x < 697 && 349 < y < 523)
-	//Plot 17: if(697 < x < 870 && 349 < y < 523)
-	//Plot 18: if(870 < x < 1043 && 349 < y < 523)
-	
-	// FOURTH ROW STARTING FROM LEFT
-	
-	//Plot 19: if(x < 175 && 523 < y < 697)
-	//Plot 20: if(175 < x < 349 && 523 < y < 697)
-	//Plot 21: if(349 < x < 523 && 523 < y < 697)
-	//Plot 22: if(523 < x < 697 && 523 < y < 697)
-	//Plot 23: if(697 < x < 870 && 523 < y < 697)
-	//Plot 24: if(870 < x < 1043 && 523 < y < 697)
-
-	//Can't actually reach the bottom on my small screen lol so the rest will have to wait...
 }
