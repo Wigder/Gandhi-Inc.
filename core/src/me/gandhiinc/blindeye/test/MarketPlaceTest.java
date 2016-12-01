62ddd2ac2f8ccf5f74c7fbbf2aeef62accd8db82
Will <will6767@gmail.com>
2016-12-01 16:16:28 +0000

    .project

---
+++ core/src/me/gandhiinc/blindeye/test/MarketPlaceTest.java
@@ -0,0 +1,189 @@
+package me.gandhiinc.blindeye.test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MarketPlaceTest {
+
+	@Test
+	public void testBuyOre() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBuyEnergy() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBuyRoboticon() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSellOre() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSellEnergy() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testProduceRoboticon() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testScratchCard() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLottery() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testOneArmBandit() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMarketOreStock() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMarketOreStock() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMarketFoodStock() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMarketFoodStock() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMarketEnergyStock() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMarketEnergyStock() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMarketRoboticonStock() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMarketRoboticonStock() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMarketOreBuyPrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMarketOreBuyPrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMarketEnergyBuyPrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMarketEnergyBuyPrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMarketOreSellPrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMarketOreSellPrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMarketEnergySellPrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMarketEnergySellPrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMarketRoboticonSellPrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMarketRoboticonSellPrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPub() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMaxOrePrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMaxOrePrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMinOrePrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMinOrePrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMaxEnergyPrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMaxEnergyPrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetMinEnergyPrice() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetMinEnergyPrice() {
+		fail("Not yet implemented");
+	}
+
+}
